c75d8c2551def35c62a4e4a13dedfff0b5e04295
Cyprian Munene <munenecyp@gmail.com>
2019-02-07 07:19:29 +0300

    delete the initial models folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
-from config import DevConfig
-# from flask_bootstrap import Bootstrap
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+from config import config_options
 
-#Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
 
-# Initializing Flask Extensions
-# bootstrap = Bootstrap(app)
 
-from app.main import views
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ app/models.py
@@ -0,0 +1,24 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self, id, description, author, publishedAt, urlToImage):
+        self.source = id
+        self.description = description
+        self.publishedAt = publishedAt
+        self.author = author
+        self.urlToImage = urlToImage
+
+
+class Sources:
+    '''
+    News class to define News Objects
+    '''
+
+    all_sources = []
+
+    def __init__(self, id, name, url):
+        self.source = id
+        self.name = name
+        self.url = url
